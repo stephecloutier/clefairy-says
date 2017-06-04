d91921b05fdb86d2613361c39e4608c168bbbd78
stephecloutier <stephecloutier@gmail.com>
2017-06-04 18:44:16 +0200

    Created multiple associative arrays for the starting screen elements

--- src/js/starting.js
+++ src/js/starting.js
@@ -5,58 +5,81 @@
  * coded by Stéphanie Cloutier
  */
 
- class CSStarting {
-     constructor(width, height) {
-         this.frames = {
-             "title": {
-                 "sx": 558,
-                 "sy": 632,
-                 "sw": 146,
-                 "sh": 60,
-                 "dx": (width - 146) / 2,
-                 "dy": 84,
-                 "dw": 146,
-                 "dh": 60,
-             },
-             "emote": {
-                 "sx": 458,
-                 "sy": 666,
-                 "sw": 30,
-                 "sh": 34,
-                 "dx": (width - 30) / 2,
-                 "dy": 186,
-                 "dw": 30,
-                 "dh": 34,
-             },
-             "clefairy": {
-                 "sx": 6,
-                 "sy": 0,
-                 "sw": 116,
-                 "sh": 120,
-                 "dx": (width - 116) / 2,
-                 "dy": 226,
-                 "dw": 116 ,
-                 "dh": 120 ,
-             },
-             "button": {
-                 "sx": 290,
-                 "sy": 540,
-                 "sw": 220,
-                 "sh": 92,
-                 "dx": (width - 220) / 2,
-                 "dy": 380,
-                 "dw": 220,
-                 "dh": 92,
-             },
+class CSStarting {
+    constructor(width, height) {
+        this.title = {
+             "sx": 558,
+             "sy": 632,
+             "sw": 146,
+             "sh": 60,
+             "dx": (width - 146) / 2,
+             "dy": 84,
+             "dw": 146,
+             "dh": 60,
+        };
+        this.emote = {
+             "sx": 458,
+             "sy": 666,
+             "sw": 30,
+             "sh": 34,
+             "dx": (width - 30) / 2,
+             "dy": 186,
+             "dw": 30,
+             "dh": 34,
+        };
+        this.clefairy = [
+            {
+                  "sx": 6,
+                  "sy": 0,
+                  "sw": 128,
+                  "sh": 124,
+                  "dx": (width - 116) / 2,
+                  "dy": 226,
+                  "dw": 128,
+                  "dh": 124,
+            },
+            {
+                "sx": 0,
+                "sy": 128,
+                "sw": 128,
+                "sh": 124,
+                "dx": (width - 128) / 2,
+                "dy": 226,
+                "dw": 128 ,
+                "dh": 124 ,
+            },
+        ];
+        this.button = {
+             "sx": 290,
+             "sy": 540,
+             "sw": 220,
+             "sh": 92,
+             "dx": (width - 220) / 2,
+             "dy": 380,
+             "dw": 220,
+             "dh": 92,
+        };
+    }
 
-         }
+     /*
+     this.frames.clefairy.animation = {
+         "max": this.frames.clefairy.length,
+         "current": 0,
      }
+     */
 
-     draw(game){
-         game.drawSpriteFromFrames(this.frames.title);
-         game.drawSpriteFromFrames(this.frames.emote);
-         game.drawSpriteFromFrames(this.frames.clefairy);
-         game.drawSpriteFromFrames(this.frames.button);
-     }
+     //this.time
 
- }
+     //this.frames.clefairy.state = {}
+
+
+
+    draw(game){
+        game.drawSpriteFromFrames(this.title);
+        game.drawSpriteFromFrames(this.emote);
+        //game.drawSpriteFromFrames(this.clefairy);
+        game.drawSpriteFromFrames(this.button);
+
+    }
+
+}
