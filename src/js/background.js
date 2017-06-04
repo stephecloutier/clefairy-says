8e840ad52f8be9334cca7bc236ca9cd643a1c6f6
stephecloutier <stephecloutier@gmail.com>
2017-06-04 14:46:39 +0200

    Created background class

---
+++ src/js/background.js
@@ -0,0 +1,25 @@
+/* clefairy-says
+ *
+ * src/js/background.js - Background class
+ *
+ * coded by Stéphanie Cloutier
+ */
+
+class CSBackground {
+    constructor(width, height) {
+        this.frame = {
+            "sx": 0,
+            "sy": 0,
+            "sw": 460,
+            "sh": 540,
+            "dx": 0,
+            "dy": 0,
+            "dw": width,
+            "dh": height,
+        };
+    }
+
+    draw(game) {
+        game.drawSpriteFromFrames(this.frame);
+    }
+}
