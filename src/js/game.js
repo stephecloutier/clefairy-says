d8ff8885773def509791c52d06d4e9d43a7dcb00
stephecloutier <stephecloutier@gmail.com>
2017-06-04 12:24:23 +0200

    Canvas initialisation, game launcher

---
+++ src/js/game.js
@@ -0,0 +1,28 @@
+/* clefairy-says
+ *
+ * /game.js - Canvas initialisation, game launcher
+ *
+ * coded by Stéphanie Cloutier
+ */
+
+let oApp = {
+    "canvas": null,
+    "context": null,
+    "width": null,
+    "height": null,
+};
+
+const setup = function90 {
+    oApp.canvas = document.querySelector("#game");
+    if(!oApp.canvas.getContext) {
+        console.error("Your browser doesn't support canvas, please update to play!");
+        return;
+    }
+    oApp.context = oApp.canvas.getContext("2d");
+    oApp.width = oApp.canvas.width;
+    oApp.height = oApp.canvas.height;
+
+    new Clefairy(oApp);
+};
+
+setup();
