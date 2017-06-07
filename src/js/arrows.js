dd55841d4fb1511a5f7f7a065982349a8b000d5b
stephecloutier <stephecloutier@gmail.com>
2017-06-07 14:25:29 +0200

    Added incrementation on dx and dy for the arrows + corrected sprite coordinates

--- src/js/arrows.js
+++ src/js/arrows.js
@@ -1,13 +1,14 @@
-class CSArrows {
-    constructor(width, height) {
-        this.sprites = {
+class CSArrow {
+    constructor(direction, index) {
+        this.direction = direction;
+        this.directions = {
             "left": {
                 "sx": 288,
                 "sy": 632,
                 "sw": 14,
                 "sh": 14,
                 "dx": 53,
-                "dy": 57,
+                "dy": 54,
                 "dw": 14,
                 "dh": 14,
             },
@@ -17,34 +18,36 @@ class CSArrows {
                 "sw": 14,
                 "sh": 14,
                 "dx": 53,
-                "dy": 57,
+                "dy": 54,
                 "dw": 14,
                 "dh": 14,
             },
             "right": {
                 "sx": 288,
-                "sy": 662,
+                "sy": 678,
                 "sw": 14,
                 "sh": 14,
                 "dx": 53,
-                "dy": 57,
+                "dy": 54,
                 "dw": 14,
                 "dh": 14,
             },
             "down": {
                 "sx": 288,
-                "sy": 678,
+                "sy": 662,
                 "sw": 14,
                 "sh": 14,
                 "dx": 53,
-                "dy": 57,
+                "dy": 54,
                 "dw": 14,
                 "dh": 14,
             },
-        }
+        };
+        this.directions[this.direction].dx += 37 * (index % 10);
+        this.directions[this.direction].dy += 40 * Math.floor(index/10);
     }
 
-    draw(game, direction) {
-        game.drawSpriteFromFrames(this.sprites[direction]);
+    draw(game) {
+        game.drawSpriteFromFrames(this.directions[this.direction]);
     }
 }
